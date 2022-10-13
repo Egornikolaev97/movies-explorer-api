fe1f764444f5d32393610386c59642c72ce12172
Egor Nikolaev <egornikolaev97@yandex.ru>
2022-10-13 21:42:05 +0600

    fix: bug with jwt_secret

--- controllers/users.js
+++ controllers/users.js
@@ -1,9 +1,8 @@
 const { NODE_ENV, JWT_SECRET } = process.env;
-const bcrypt = require('bcryptjs');
 const jwt = require('jsonwebtoken');
+const bcrypt = require('bcryptjs');
 const User = require('../models/user');
 const NotFoundError = require('../utils/NotFoundError');
-const { JWT_SECRET_DEV } = require('../configuration/index');
 const ConflictError = require('../utils/ConflictError');
 const BadRequestError = require('../utils/BadRequestError');
 const {
@@ -77,7 +76,7 @@ module.exports.login = (req, res, next) => {
   const { email, password } = req.body;
   return User.findUserByCredentials(email, password)
     .then((user) => {
-      const token = jwt.sign({ _id: user._id }, NODE_ENV === 'production' ? JWT_SECRET : JWT_SECRET_DEV, { expiresIn: '7d' });
+      const token = jwt.sign({ _id: user._id }, NODE_ENV === 'production' ? JWT_SECRET : 'secret-key', { expiresIn: '7d' });
       res.send({ token });
     })
     .catch(next);
