c7810038966686e6378fe32d90cb3ab4c6b07a73
Egor Nikolaev <egornikolaev97@yandex.ru>
2023-01-21 22:37:08 +0600

    fix cors error

--- middlewares/cors.js
+++ middlewares/cors.js
@@ -1,22 +1,30 @@
+// домены, разрешенные для крос-доменных запросов
 const allowedCors = [
+  'http://https://api.nikolaev.movies.nomoredomains.icu/',
   'https://https://api.nikolaev.movies.nomoredomains.icu/',
-  'https://localhost:3000',
-  'https://localhost:3500',
+  'http://https://nikolaev.movies.nomoredomains.icu/',
+  'https://https://nikolaev.movies.nomoredomains.icu/',
+  'http://localhost:3000/',
+  'https://localhost:3000/',
+  'http://localhost:3500/',
+  'https://localhost:3500/',
 ];
 
 module.exports = (req, res, next) => {
-  const { origin } = req.headers;
-  const { method } = req;
-  const DEFAULT_ALLOWED_METHODS = 'GET,HEAD,PUT,PATCH,POST,DELETE';
+  const { origin } = req.headers; // сохраняем источник в origin
+  const { method } = req; // сохраняем hhtp
   const requestHeaders = req.headers['access-control-request-headers'];
+  const DEFAULT_ALLOWED_METHODS = 'GET,HEAD,PUT,PATCH,POST,DELETE';
+  // проверка, разрешен ли источник запроса
   if (allowedCors.includes(origin)) {
+    // разрешаем запросы с этого источника
     res.header('Access-Control-Allow-Origin', origin);
-    res.header('Access-Control-Allow-Credentials', true);
   }
   if (method === 'OPTIONS') {
     res.header('Access-Control-Allow-Methods', DEFAULT_ALLOWED_METHODS);
     res.header('Access-Control-Allow-Headers', requestHeaders);
     return res.end();
   }
+
   return next();
 };
